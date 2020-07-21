0272298efc86eec8dc26287b5f4aaea57f11279a
Scott Wilson <scottwilson1020@gmail.com>
2020-07-21 21:53:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,10 +12,10 @@ def shipping_manifest
   
   the_manifest = {
     
-    "whales bone corsets" => 5, 
-    "porcelain vases" => 2,
-    "oil paintings" => 3,
-    "muskets" => 2,
+    "whales bone corsets" => 5 
+    "porcelain vases" => 2
+    "oil paintings" => 3
+    "muskets" => 2
     "gun powder" => 4
     
   }

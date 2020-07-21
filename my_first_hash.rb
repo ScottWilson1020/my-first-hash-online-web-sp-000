5e9aba3c673af20cb1a9352ae27a5ddb9cad4896
Scott Wilson <scottwilson1020@gmail.com>
2020-07-21 22:23:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -43,7 +43,8 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
- shipping_manifest ["gun powder"] => 4, 
+shipping_manifest
+ shipping_manifest ["gun powder"] => 4
 
   # return the shipping_manifest hash below
 shipping_manifest
